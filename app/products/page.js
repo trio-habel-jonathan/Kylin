3711929727fee47fd6a000b81a7476f9f3d12e6e
Habib Abdillah <mhabibabdillah083@gmail.com>
2025-07-12 21:26:30 +0700

    update fix npm run build

--- app/products/page.js
+++ app/products/page.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero from "./partials/Hero";
-import { motion } from "framer-motion";
 import OurProjects from "./partials/OurProjects";
 import {
   TrendingUp,
